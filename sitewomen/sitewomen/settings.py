a9d583680c9d50209c87f48860f64dbd5b3082ba
stackman <228bobmarley228@gmail.com>
2024-02-23 17:01:26 +0200

    change SQLite to postgreSQL

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -72,10 +72,22 @@ WSGI_APPLICATION = 'sitewomen.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'stackman',
+        'USER': 'postgres',
+        'PASSWORD': 'dima282002',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
