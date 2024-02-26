15deec9fe27853729168aba16c590a2bc224e83e
stackman <228bobmarley228@gmail.com>
2024-02-26 21:46:56 +0200

    new url

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, re_path, register_converter
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, \
@@ -7,12 +9,11 @@ urlpatterns = [
     path('', views.index, name='home'),  # http://127.0.0.1:8000
     path('about/', views.about, name='about'),  # http://127.0.0.1:8000/about/
     path('contact/', views.contact, name='contact'),
+    path('addpage/', views.addpage, name='addpage'),
     path('post/<int:post_id>/', views.show_post, name='post'),  # http://127.0.0.1:8000/post/1/
     path('category/<int:cat_id>/', views.show_category, name='category'),  # http://127.0.0.1:8000/category/1/
 
     path('register/', views.register, name='register'),
-    # path('login/', AuthLoginView.as_view(), name='login'),
-    # path('login/', LoginView.as_view(), name='login'),
     path('login/', views.login_user, name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
